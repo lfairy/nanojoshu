e7463dcd139a05a9c9c59328ac51912b692bc16e
Chris Wong <lambda.fairy@gmail.com>
2017-04-18 20:15:51 +1200

    Use absolute path for Bundler

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 while true
 do
     echo
-    bundle exec dotenv ./bot.rb
+    ~/.rbenv/shims/bundle exec dotenv ./bot.rb
     secs=$(( 3600 + (RANDOM % 64) ))
     printf 'Sleeping for %d seconds\n' $secs
     sleep $secs
